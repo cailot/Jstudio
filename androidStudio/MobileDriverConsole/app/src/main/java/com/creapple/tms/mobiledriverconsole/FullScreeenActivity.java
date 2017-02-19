dbd66ca5dc6255d57b877675985b3c0f8bd4380a
cailot <cailot@telstra.com>
2017-02-19 21:15:47 +1100

    display action bar info - battery, times, etc

--- androidStudio/MobileDriverConsole/app/src/main/java/com/creapple/tms/mobiledriverconsole/FullScreeenActivity.java
+++ androidStudio/MobileDriverConsole/app/src/main/java/com/creapple/tms/mobiledriverconsole/FullScreeenActivity.java
@@ -23,9 +23,9 @@ public class FullScreeenActivity extends AppCompatActivity {
         final int flags =
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                 | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                  | View.SYSTEM_UI_FLAG_IMMERSIVE;
         // This work only for android 4.4+
         if(currentApiVersion > Build.VERSION_CODES.KITKAT)
@@ -96,9 +96,9 @@ public class FullScreeenActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE);
         }
     }
--- androidStudio/MobileDriverConsole/app/src/main/java/com/creapple/tms/mobiledriverconsole/MDCMainActivity.java
+++ androidStudio/MobileDriverConsole/app/src/main/java/com/creapple/tms/mobiledriverconsole/MDCMainActivity.java
@@ -138,9 +138,9 @@ public class MDCMainActivity extends AppCompatActivity implements GoogleApiClien
         final int flags =
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         // This work only for android 4.4+
